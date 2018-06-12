35f3d2484d7fd388a7fd63b3532034a083c8208e
Andres Ossa <raveneyex@gmail.com>
2018-06-12 00:09:20 -0500

    ☢  Number Transmutation Logic WIP. Still Wonky

--- src/app/services/transmutation.service.ts
+++ src/app/services/transmutation.service.ts
@@ -3,6 +3,51 @@ import TransmutatedPair from '../types/TransmutatedPair';
 
 let limit;
 
+const TOKENS = {
+    0: 'zero',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'fifteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen',
+    20: 'twenty',
+    30: 'thirty',
+    40: 'forty',
+    50: 'fifty',
+    60: 'sixty',
+    70: 'seventy',
+    80: 'eighty',
+    90: 'ninety',
+    100: 'hundred and',
+    1000: 'thousand',
+    1000000: 'million',
+    1000000000: 'billion',
+    1000000000000: 'trillion',
+    1000000000000000: 'quatrillion',
+    /**
+     * Quintillion cannot be safely represented in JS.
+     * Here to showcase how the algorithm can be expanded beyond the system limit.
+     */
+    QUINTILLION: 'quintillion',
+    MINUS: 'Negative'
+};
+
+const MAGNITUDES = [100, 1000, 1000000, 1000000000, 1000000000000, 1000000000000000];
+
 @Injectable()
 export class TransmutationService {
     get limit(): number {
@@ -20,6 +65,63 @@ export class TransmutationService {
     }
 
     public transmutate(input: number): TransmutatedPair {
-        return { num: input, words: input.toString() };
+        debugger;
+        const words = this._toWords(input);
+        return { num: input, words };
+    }
+
+    private _toWords(number: number, words = []) {
+        debugger;
+        if (number < 100) {
+            const value = this._lessThan100(number);
+            words.push(value);
+        } else if (number === 100) {
+            words.push('one hundred');
+        } else if (number > 100) {
+            const values = this._moreThan100(number);
+            debugger;
+            words = [...words, ...values];
+        }
+        return words.join(" ");
+    }
+
+    private _lessThan100(number: number, words = []): string {
+        if (number < 0) {
+            words.push(TOKENS.MINUS);
+            number = Math.abs(number);
+        }
+
+        if (number <= 20) {
+            words.push(TOKENS[number]);
+        } else if (number < 100) {
+            const reminder = number % 10;
+            words.push(TOKENS[number - reminder]);
+            words.push(`-${TOKENS[reminder]}`);
+        }
+
+        return words.join('');
+    }
+
+    private _moreThan100(number: number, words = []): string[] {
+        for (let i = 1; i <= MAGNITUDES.length; i += 1) {
+            debugger;
+            // Note: intentionally skipping first magnitude.
+            const MAGNITUDE = MAGNITUDES[i];
+            const DIVIDER = MAGNITUDES[i - 1];
+
+            if (number < MAGNITUDE) {
+                const remainder = number % DIVIDER;
+                const word = `${this._toWords(Math.floor(number / DIVIDER))} ${TOKENS[DIVIDER]}`;
+                words.push(word);
+
+                if (remainder) {
+                    const remainderWords = this._toWords(remainder);
+                    words.push(remainderWords);
+                }
+
+                break;
+            }
+        }
+        return words;
     }
 }
